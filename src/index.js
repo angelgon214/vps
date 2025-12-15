891f35f57a37b4685c2d07fad0ed5c65b0dfb296
angelgon214 <angelgonzalezsantafe@gmail.com>
2025-12-15 14:55:05 -0600

    HTML frontend interface

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use('/api', apiRoutes);
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(process.cwd(), 'public', 'index.html'));
 });
 
 app.use((err, req, res, next) => {
